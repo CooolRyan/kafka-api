219f7db8325207729e6aa1ebbffb2236d6e60fc6
CooolRyan <74342019+CooolRyan@users.noreply.github.com>
2025-10-23 17:28:05 +0900

    chore : kafka 버전 병경에 따른 세팅 값 롤백

--- kafka-consumer/src/main/java/apache/kafkaconsumer/service/GCMonitoringService.java
+++ kafka-consumer/src/main/java/apache/kafkaconsumer/service/GCMonitoringService.java
@@ -14,7 +14,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-@Service
+// @Service  // GC 모니터링 완전 비활성화
 @Slf4j
 public class GCMonitoringService {
 
